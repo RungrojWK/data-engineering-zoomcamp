24e7ff6eb153e4694f3b392beba97b87ebf0e09c
Rungroj Wongkaeo <63869763+RungrojWK@users.noreply.github.com>
2025-01-21 21:28:02 +0700

    Initial week1 answer Q1 to Q6.

---
+++ 01-docker-terraform/2_docker_sql/script.sh
@@ -0,0 +1,58 @@
+docker run -it \  
+  --network=pg-network \
+  --name pg-database \
+  -e POSTGRES_USER="root" \
+  -e POSTGRES_PASSWORD="1234" \
+  -e POSTGRES_DB="ny_taxi" \
+  -v d:/Devspace/2_Course/data_engineer_bootcamp_datatalks/data-engineering-zoomcamp/01-docker-terraform/2_docker_sql/ny_taxi_postgres_data:/var/lib/postgresql/data \
+  -p 5432:5432 \
+  postgres:13
+
+docker run -d \
+  --network=pg-network \
+  --name pgadmin-1 \
+  -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+  -e PGADMIN_DEFAULT_PASSWORD="root" \
+  -p 8080:80 \
+  dpage/pgadmin4
+
+URL="http://172.28.112.1:8000/green_tripdata_2019-10.csv"
+
+# Local machine
+python ingest_data.py \
+  --user=root \
+  --password=1234 \
+  --host=localhost \
+  --port=5432 \
+  --db=ny_taxi \
+  --table_name=green_taxi_trips
+  --url=${URL}
+
+docker build -t taxi_ingest:v003 .
+
+# Docker build
+URL="http://172.28.112.1:8000/green_tripdata_2019-10.csv"
+
+docker run -it \
+  taxi_ingest:v002 \
+    --network=pg-network \
+    --user=root \
+    --password=1234 \
+    --host=pg-database \
+    --port=5432 \
+    --db=ny_taxi \
+    --table_name=green_taxi_trips \
+    --url=${URL}
+
+URL="http://172.28.112.1:8000/taxi_zone_lookup.csv"
+
+docker run -it \
+  taxi_ingest:v003 \
+    --network=pg-network \
+    --user=root \
+    --password=1234 \
+    --host=pg-database \
+    --port=5432 \
+    --db=ny_taxi \
+    --table_name=taxi_zone \
+    --url=${URL}
